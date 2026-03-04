9aed867843e8e1874e0d8fb75829b775cfcf7ef6
mathiasmihalceadev <mathias.mihalcea@gmail.com>
2026-03-04 11:44:58 +0200

    Version update

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname, "src"),
-    };
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
